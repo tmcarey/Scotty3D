3d7fae7d9609bd425466db138e7bf9456090de73
Thomas Carey <thomas@thomascarey.us>
2021-12-03 19:07:42 -0500

    A4 Done

--- src/student/particles.cpp
+++ src/student/particles.cpp
@@ -7,17 +7,48 @@ bool Scene_Particles::Particle::update(const PT::Object& scene, float dt, float 
     // TODO(Animation): Task 4
 
     // Compute the trajectory of this particle for the next dt seconds.
+    float remaining = dt;
 
     // (1) Build a ray representing the particle's path if it travelled at constant velocity.
+    for(uint i = 0; i < 5 && remaining > 0.0001f; i++){
+        Ray trajectory;
+        trajectory.point = pos;
+        trajectory.dir = velocity.unit();
 
-    // (2) Intersect the ray with the scene and account for collisions. Be careful when placing
-    // collision points using the particle radius. Move the particle to its next position.
+        // (2) Intersect the ray with the scene and account for collisions. Be careful when placing
+        // collision points using the particle radius. Move the particle to its next position.
+        PT::Trace hit = scene.hit(trajectory);
+
+        Vec3 o = pos;
+        Vec3 v = velocity.unit();
+        Vec3 p = hit.position;
+        Vec3 n = hit.normal;
+        if(dot(v,n) < 0.0f){
+            n = -n;
+        }
+        float dist = ((dot(p,n) - dot(o,n) - (radius/ 2.0f)) / dot(v, n));
+        if(!hit.hit){
+            dist = remaining * velocity.norm();
+        }
+        if(dist >= remaining * velocity.norm()){
+            pos = trajectory.at(remaining * velocity.norm());
+            velocity += (remaining) * acceleration;
+            break;
+        }else{
+            pos = trajectory.at(dist);
+            velocity = (v - 2 * dot(v, n) * n) * velocity.norm();
+            velocity += (dist / velocity.norm()) * acceleration;
+            remaining -= dist;
+        }
+    }
+    
 
     // (3) Account for acceleration due to gravity.
 
     // (4) Repeat until the entire time step has been consumed.
 
     // (5) Decrease the particle's age and return whether it should die.
+    age -= dt;
 
-    return false;
+    return age >= 0;
 }
--- src/student/skeleton.cpp
+++ src/student/skeleton.cpp
@@ -1,12 +1,20 @@
 
 #include "../scene/skeleton.h"
+#include "../rays/pathtracer.h"
 
 Vec3 closest_on_line_segment(Vec3 start, Vec3 end, Vec3 point) {
 
     // TODO(Animation): Task 3
 
     // Return the closest point to 'point' on the line segment from start to end
-    return Vec3{};
+    float t = dot(point - start, (end - start).unit());
+    if (t >= (end-start).norm()){
+        return end;
+    }else if (t <= 0.0f){
+        return start;
+    }else {
+        return ((end - start).unit() * t) + start;
+    }
 }
 
 Mat4 Joint::joint_to_bind() const {
@@ -20,7 +28,15 @@ Mat4 Joint::joint_to_bind() const {
 
     // You will need to traverse the joint heirarchy. This should
     // not take into account Skeleton::base_pos
-    return Mat4::I;
+    Mat4 transform = Mat4();
+    const Joint *j = this;
+    while(!j->is_root())
+    {   
+        Mat4 translation = Mat4::translate(j->parent->extent);
+        transform = translation * transform;
+        j = j->parent;
+    }
+    return transform;
 }
 
 Mat4 Joint::joint_to_posed() const {
@@ -32,7 +48,16 @@ Mat4 Joint::joint_to_posed() const {
 
     // You will need to traverse the joint heirarchy. This should
     // not take into account Skeleton::base_pos
-    return Mat4::I;
+    const Joint *j = this;
+    Mat4 transform = Mat4::euler(j->pose);
+    while(!j->is_root())
+    {   
+        Mat4 rotation = Mat4::euler(j->parent->pose);
+        Mat4 translation = Mat4::translate(j->parent->extent);
+        transform = rotation * translation * transform;
+        j = j->parent;
+    }
+    return transform;
 }
 
 Vec3 Skeleton::end_of(Joint* j) {
@@ -41,7 +66,7 @@ Vec3 Skeleton::end_of(Joint* j) {
 
     // Return the bind position of the endpoint of joint j in object space.
     // This should take into account Skeleton::base_pos.
-    return Vec3{};
+    return joint_to_bind(j) * j->extent;
 }
 
 Vec3 Skeleton::posed_end_of(Joint* j) {
@@ -50,7 +75,7 @@ Vec3 Skeleton::posed_end_of(Joint* j) {
 
     // Return the posed position of the endpoint of joint j in object space.
     // This should take into account Skeleton::base_pos.
-    return Vec3{};
+    return joint_to_posed(j) * j->extent;
 }
 
 Mat4 Skeleton::joint_to_bind(const Joint* j) const {
@@ -59,7 +84,7 @@ Mat4 Skeleton::joint_to_bind(const Joint* j) const {
 
     // Return a matrix transforming points in joint j's space to object space in
     // bind position. This should take into account Skeleton::base_pos.
-    return Mat4::I;
+    return Mat4::translate(base_pos) * j->joint_to_bind();
 }
 
 Mat4 Skeleton::joint_to_posed(const Joint* j) const {
@@ -68,7 +93,7 @@ Mat4 Skeleton::joint_to_posed(const Joint* j) const {
 
     // Return a matrix transforming points in joint j's space to object space with
     // poses. This should take into account Skeleton::base_pos.
-    return Mat4::I;
+    return  Mat4::translate(base_pos) * j->joint_to_posed();
 }
 
 void Skeleton::find_joints(const GL::Mesh& mesh, std::vector<std::vector<Joint*>>& map) {
@@ -82,11 +107,23 @@ void Skeleton::find_joints(const GL::Mesh& mesh, std::vector<std::vector<Joint*>
     const std::vector<GL::Mesh::Vert>& verts = mesh.verts();
     map.resize(verts.size());
 
+    for(uint i = 0; i < verts.size(); i++){
+        map[i].resize(0);
+    }
     // For each i in [0, verts.size()), map[i] should contain the list of joints that
     // effect vertex i. Note that i is NOT Vert::id! i is the index in verts.
 
     for_joints([&](Joint* j) {
         // What vertices does joint j effect?
+        Vec3 jointStart = joint_to_bind(j) * Vec3(0, 0, 0);
+        Vec3 jointEnd = (joint_to_bind(j) * j->extent);
+        for(uint i = 0; i < verts.size(); i++){
+            Vec3 point = verts[i].pos;
+            Vec3 pointOnJoint = closest_on_line_segment(jointStart, jointEnd, point);
+            if(((point - pointOnJoint).norm()) <= j->radius){
+                map[i].push_back(j);
+            }
+        }
     });
 }
 
@@ -103,14 +140,33 @@ void Skeleton::skin(const GL::Mesh& input, GL::Mesh& output,
     // Currently, this just copies the input to the output without modification.
 
     std::vector<GL::Mesh::Vert> verts = input.verts();
+    std::vector<GL::Mesh::Vert> newVerts;
+    newVerts.resize(verts.size());
 
     for(size_t i = 0; i < verts.size(); i++) {
-
+        Vec3 sum = Vec3(0);
+        float denom = 0;
         // Skin vertex i. Note that its position is given in object bind space.
+        if(map[i].size() < 1){
+            sum = verts[i].pos;
+            denom = 1.0f;
+        }
+        for(auto jit = map[i].begin(); jit < map[i].end(); jit++){
+            Joint *j = *jit;
+            Vec3 jointStart = joint_to_bind(j) * Vec3(0);
+            Vec3 jointEnd = joint_to_bind(j) * j->extent;
+            Vec3 pointOnJoint = closest_on_line_segment(jointStart, jointEnd, verts[i].pos);
+            Mat4 bindToPose = joint_to_posed(j) * joint_to_bind(j).inverse();
+            float distInv = 1.0f / (verts[i].pos - pointOnJoint).norm();
+            sum += distInv * (bindToPose * verts[i].pos);
+            denom += distInv;
+        }
+        newVerts[i] = verts[i];
+        newVerts[i].pos = sum / denom;
     }
 
     std::vector<GL::Mesh::Index> idxs = input.indices();
-    output.recreate(std::move(verts), std::move(idxs));
+    output.recreate(std::move(newVerts), std::move(idxs));
 }
 
 void Joint::compute_gradient(Vec3 target, Vec3 current) {
@@ -123,11 +179,38 @@ void Joint::compute_gradient(Vec3 target, Vec3 current) {
 
     // Target is the position of the IK handle in skeleton space.
     // Current is the end position of the IK'd joint in skeleton space.
+    // x gradient
+    if(!is_root()){
+        parent->compute_gradient(target, current);
+    }
+    Vec3 delta = current - target;
+    Vec3 Jx = cross(joint_to_posed() * Vec3(1, 0, 0), current - (joint_to_posed() * extent));
+    float x = dot(Jx, delta);
+    Vec3 Jy = cross(joint_to_posed() * Vec3(0, 1, 0), current - (joint_to_posed() * extent));
+    float y = dot(Jy, delta);
+    Vec3 Jz = cross(joint_to_posed() * Vec3(0, 0, 1), current - (joint_to_posed() * extent));
+    float z = dot(Jz, delta);
+    angle_gradient = Vec3(x, y, z);
 }
 
 void Skeleton::step_ik(std::vector<IK_Handle*> active_handles) {
 
     // TODO(Animation): Task 2
+    float T = 0.03f;
 
     // Do several iterations of Jacobian Transpose gradient descent for IK
+    for (uint i = 0; i < 100; i++){
+        Vec3 gradient = Vec3(0);
+        for (uint handle = 0; handle < active_handles.size(); handle++){
+            Joint *j = active_handles[handle]->joint;
+            j->compute_gradient(active_handles[handle]->target, j->joint_to_posed() * j->extent);
+            while(true){
+                j->pose -= j->angle_gradient * T;
+                if(j->is_root()){
+                    break;
+                }
+                j = j->parent;
+            }
+        }
+    }
 }
